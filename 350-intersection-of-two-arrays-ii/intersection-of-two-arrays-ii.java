fdfd761ecd567d776fecb056ea1914bcecc4903f
Aditya Deshmane <adityadeshmane41@gmail.com>
2026-08-08 23:15:56 +0530

    Time: 1 ms (98.47%) | Memory: 44.5 MB (94.15%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
+        int[] count=new int[1001];
         ArrayList<Integer> list=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
 
-       for(int i=0;i<nums1.length;i++){
-            map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
-       }
+        for(int num:nums1){
+            count[num]++;
+        }
 
-       for(int i=0;i<nums2.length;i++){
-            if(map.containsKey(nums2[i]) && map.get(nums2[i])>0){
-                list.add(nums2[i]);
-                map.put(nums2[i],map.get(nums2[i])-1);
+        for(int num:nums2){
+            if(count[num]>0){
+                list.add(num);
+                count[num]--;
             }
-       }
-       int[] ans=new int[list.size()];
-       for(int i=0;i<list.size();i++){
-        ans[i]=list.get(i);
-       }
-       return ans;
+        }
+        int[] arr=new int[list.size()];
+
+        for(int i=0;i<list.size();i++){
+            arr[i]=list.get(i);
+        }
+        return arr;
     }
 }

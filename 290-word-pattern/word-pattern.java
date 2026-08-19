3c967a4e5e0322f3183590a1a724d2ee5534f4da
Aditya Deshmane <adityadeshmane41@gmail.com>
2026-08-20 00:17:30 +0530

    Time: 2 ms (12.04%) | Memory: 42.7 MB (56.16%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr=s.trim().split("\\s+");
+        HashMap<Character,String> map=new HashMap<>();
+
+        if(pattern.length()!=arr.length){
+            return false;
+        }
+        for(int i=0;i<pattern.length();i++){
+            char ch=pattern.charAt(i);
+            if(map.containsKey(ch)){
+                if(!map.get(ch).equals(arr[i])){
+                    return false;
+                }
+            }
+            else{
+                if(map.containsValue(arr[i])) return false;
+
+                map.put(ch,arr[i]);
+            }
+        }
+        return true;
+    }
+}

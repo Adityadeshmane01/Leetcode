767ed888a605221d47a376035b765dc0ad1fc7b2
Aditya Deshmane <adityadeshmane41@gmail.com>
2026-08-26 09:58:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (99.53%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,13 +1,9 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        HashSet<Character> chars=new HashSet<>();
         int count=0;
-        for(char ch:jewels.toCharArray()){
-            chars.add(ch);
-        }
 
-        for(char ch:stones.toCharArray()){
-            if(chars.contains(ch)){
+        for(int i=0;i<stones.length();i++){
+            if(jewels.indexOf(stones.charAt(i))!=-1){
                 count++;
             }
         }

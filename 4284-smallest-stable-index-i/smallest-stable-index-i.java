7e9c5a80555e02e056c4141de002d391e4dec2fb
Aditya Deshmane <adityadeshmane41@gmail.com>
2026-09-04 10:27:03 +0530

    Time: 1 ms (99.79%) | Memory: 46.4 MB (32.13%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        for(int i=0;i<nums.length;i++){
+            int max=Integer.MIN_VALUE;
+            int min=Integer.MAX_VALUE;
+
+            for(int j=0;j<=i;j++){
+                max=Math.max(max,nums[j]);
+            }
+            for(int p=i;p<nums.length;p++){
+                min=Math.min(min,nums[p]);
+            }
+            if((max-min)<=k){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
